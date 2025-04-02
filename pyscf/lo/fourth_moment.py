32496a009f5a92a043d7b4fc40b693afaf22290d
Jonas Greiner <jongr@kemi.dtu.dk>
2025-04-02 14:22:50 +0200

    Performance improvements for Fourth-Moment localization.

--- pyscf/lo/fourth_moment.py
+++ pyscf/lo/fourth_moment.py
@@ -125,14 +125,22 @@ class FourthMoment(boys.OrbitalLocalizer):
         # xxxx + xxyy + xxzz + yyxx + yyyy + zzzz + zzxx + zzyy + zzzz
         r4_ints = reduce(lib.dot, (mo_coeff.conj().T, self.r4_ao_ints, mo_coeff))
 
+        # get diagonals, explicitly copy these to avoid jumps in memory
+        r_ints_d = r_ints.diagonal(axis1=1, axis2=2).copy()
+        rr_ints_d = rr_ints.diagonal(axis1=2, axis2=3).copy()
+        r3_ints_d = r3_ints.diagonal(axis1=1, axis2=2).copy()
+        r4_ints_d = r4_ints.diagonal().copy()
+        xx_ints = numpy.moveaxis(rr_ints.diagonal(axis1=0, axis2=1), -1, 0)
+        xx_ints_d = numpy.moveaxis(rr_ints_d.diagonal(axis1=0, axis2=1), -1, 0)
+
         # calculate orbital fourth moment
         if self.exponent > 1:
             fou_mom = (
-                numpy.einsum("ii->i", r4_ints)
-                - 4 * numpy.einsum("xii,xii->i", r3_ints, r_ints)
-                + 2 * numpy.einsum("xxii,yii,yii->i", rr_ints, r_ints, r_ints)
-                + 4 * numpy.einsum("xyii,xii,yii->i", rr_ints, r_ints, r_ints)
-                - 3 * numpy.einsum("xii,xii->i", r_ints, r_ints) ** 2
+                r4_ints_d
+                - 4 * numpy.einsum("xi,xi->i", r3_ints_d, r_ints_d)
+                + 2 * numpy.einsum("xi,yi,yi->i", xx_ints_d, r_ints_d, r_ints_d)
+                + 4 * numpy.einsum("xyi,xi,yi->i", rr_ints_d, r_ints_d, r_ints_d)
+                - 3 * numpy.einsum("xi,xi->i", r_ints_d, r_ints_d) ** 2
             )
             f0 = fou_mom ** (self.exponent - 1)
         else:
@@ -142,20 +150,20 @@ class FourthMoment(boys.OrbitalLocalizer):
         rrr_intermed = (
             -8
             * numpy.einsum(
-                "xxii,yii->yi", rr_ints, r_ints
+                "xi,yi->yi", xx_ints_d, r_ints_d
             )  # missing for kappa @ f1 in hx in paper
-            + 8 * numpy.einsum("xii->xi", r3_ints)
-            + 24 * numpy.einsum("xii,xii,yii->yi", r_ints, r_ints, r_ints)
-            - 16 * numpy.einsum("xyii,xii->yi", rr_ints, r_ints)
+            + 8 * r3_ints_d
+            + 24 * numpy.einsum("xi,xi,yi->yi", r_ints_d, r_ints_d, r_ints_d)
+            - 16 * numpy.einsum("xyi,xi->yi", rr_ints_d, r_ints_d)
         )
 
         # gradient
         g = numpy.zeros(2 * (self.mo_coeff.shape[1],), dtype=numpy.float64)
         g -= 2 * numpy.einsum("ij,j->ij", r4_ints, f0)  # additional factor 2
-        g -= 8 * numpy.einsum("xyij,j,xjj,yjj->ij", rr_ints, f0, r_ints, r_ints)
+        g -= 8 * numpy.einsum("xyij,j,xj,yj->ij", rr_ints, f0, r_ints_d, r_ints_d)
         g += numpy.einsum("xij,j,xj->ij", r_ints, f0, rrr_intermed)
-        g -= 4 * numpy.einsum("xxij,j,yjj,yjj->ij", rr_ints, f0, r_ints, r_ints)
-        g += 8 * numpy.einsum("xij,j,xjj->ij", r3_ints, f0, r_ints)
+        g -= 4 * numpy.einsum("xij,j,yj,yj->ij", xx_ints, f0, r_ints_d, r_ints_d)
+        g += 8 * numpy.einsum("xij,j,xj->ij", r3_ints, f0, r_ints_d)
 
         full_g = -self.exponent * (g - g.T)
 
@@ -163,36 +171,36 @@ class FourthMoment(boys.OrbitalLocalizer):
 
         # hessian diagonal
         h_diag = numpy.zeros(2 * (self.mo_coeff.shape[1],), dtype=numpy.float64)
-        h_diag += 16 * numpy.einsum("xij,xij,j,yyjj->ij", r_ints, r_ints, f0, rr_ints)
-        h_diag += 32 * numpy.einsum("xxij,yij,j,yjj->ij", rr_ints, r_ints, f0, r_ints)
+        h_diag += 16 * numpy.einsum("xij,xij,j,yj->ij", r_ints, r_ints, f0, xx_ints_d)
+        h_diag += 32 * numpy.einsum("xij,yij,j,yj->ij", xx_ints, r_ints, f0, r_ints_d)
         h_diag -= 32 * numpy.einsum("xij,xij,j->ij", r_ints, r3_ints, f0)
-        h_diag += 64 * numpy.einsum("xij,xyij,j,yjj->ij", r_ints, rr_ints, f0, r_ints)
-        h_diag += 32 * numpy.einsum("xij,yij,j,xyjj->ij", r_ints, r_ints, f0, rr_ints)
+        h_diag += 64 * numpy.einsum("xij,xyij,j,yj->ij", r_ints, rr_ints, f0, r_ints_d)
+        h_diag += 32 * numpy.einsum("xij,yij,j,xyj->ij", r_ints, r_ints, f0, rr_ints_d)
         h_diag -= 96 * numpy.einsum(
-            "xij,yij,j,xjj,yjj->ij", r_ints, r_ints, f0, r_ints, r_ints
+            "xij,yij,j,xj,yj->ij", r_ints, r_ints, f0, r_ints_d, r_ints_d
         )
         h_diag -= 48 * numpy.einsum(
-            "xij,xij,j,yjj,yjj->ij", r_ints, r_ints, f0, r_ints, r_ints
+            "xij,xij,j,yj,yj->ij", r_ints, r_ints, f0, r_ints_d, r_ints_d
         )
 
-        r4_f0 = numpy.einsum("ii,j->ij", r4_ints, f0)
+        r4_f0 = numpy.einsum("i,j->ij", r4_ints_d, f0)
         h_diag += 2 * (r4_f0 - numpy.einsum("ii->i...", r4_f0))
-        xy_f0_x_y = numpy.einsum("xyii,j,xjj,yjj->ij", rr_ints, f0, r_ints, r_ints)
+        xy_f0_x_y = numpy.einsum("xyi,j,xj,yj->ij", rr_ints_d, f0, r_ints_d, r_ints_d)
         h_diag += 8 * (xy_f0_x_y - numpy.einsum("ii->i...", xy_f0_x_y))
-        r_f0_rrr_intermed = numpy.einsum("xii,j,xj->ij", r_ints, f0, rrr_intermed)
+        r_f0_rrr_intermed = numpy.einsum("xi,j,xj->ij", r_ints_d, f0, rrr_intermed)
         h_diag -= r_f0_rrr_intermed - numpy.einsum("ii->i...", r_f0_rrr_intermed)
-        xx_f0_y_y = numpy.einsum("xxii,j,yjj,yjj->ij", rr_ints, f0, r_ints, r_ints)
+        xx_f0_y_y = numpy.einsum("xxi,j,yj,yj->ij", rr_ints_d, f0, r_ints_d, r_ints_d)
         h_diag += 4 * (xx_f0_y_y - numpy.einsum("ii->i...", xx_f0_y_y))
-        r3_f0_r = numpy.einsum("xii,j,xjj->ij", r3_ints, f0, r_ints)
+        r3_f0_r = numpy.einsum("xi,j,xj->ij", r3_ints_d, f0, r_ints_d)
         h_diag -= 8 * (r3_f0_r - numpy.einsum("ii->i...", r3_f0_r))
 
         if self.exponent > 1:
             kappa_f1 = numpy.zeros(2 * (self.mo_coeff.shape[1],), dtype=numpy.float64)
             kappa_f1 += 2 * r4_ints
-            kappa_f1 += 8 * numpy.einsum("xii,yii,xyij->ij", r_ints, r_ints, rr_ints)
+            kappa_f1 += 8 * numpy.einsum("xi,yi,xyij->ij", r_ints_d, r_ints_d, rr_ints)
             kappa_f1 -= numpy.einsum("xi,xij->ij", rrr_intermed, r_ints)
-            kappa_f1 -= 8 * numpy.einsum("xii,xij->ij", r_ints, r3_ints)
-            kappa_f1 += 4 * numpy.einsum("xii,xii,yyij->ij", r_ints, r_ints, rr_ints)
+            kappa_f1 -= 8 * numpy.einsum("xi,xij->ij", r_ints_d, r3_ints)
+            kappa_f1 += 4 * numpy.einsum("xi,xi,yij->ij", r_ints_d, r_ints_d, xx_ints)
 
             if self.exponent > 2:
                 h_diag += (
@@ -205,74 +213,182 @@ class FourthMoment(boys.OrbitalLocalizer):
 
         h_diag = self.exponent * self.pack_uniq_var(h_diag + h_diag.T)
 
+        # get intermediates
+        if self.exponent > 1:
+            x_yy_ints_d_x_ints_d = numpy.einsum("yi,xi->xi", xx_ints_d, r_ints_d)
+            x_x_ints_d_y_ints_d_y_ints_d = numpy.einsum("xi,yi,yi->xi", r_ints_d, r_ints_d, r_ints_d)
+            x_xy_ints_d_y_ints_d = numpy.einsum("xyi,yi->xi", rr_ints_d, r_ints_d)
+            interm1 = 8 * (
+                x_yy_ints_d_x_ints_d 
+                - r3_ints_d 
+                - 3 * x_x_ints_d_y_ints_d_y_ints_d 
+                + 2 * x_xy_ints_d_y_ints_d
+            )
+
+        x_f0_xxx_intermed = numpy.einsum("j,xj->xj", f0, rrr_intermed)
+        x_yy_ints_f0_x_ints_d = numpy.einsum(
+            "xij,j,yj->yij", 
+            xx_ints, 
+            f0, 
+            r_ints_d, 
+            optimize=['einsum_path', (1, 2), (0, 1)],
+        )
+        x_xy_ints_f0_y_ints_d = numpy.einsum(
+            "xyij,j,xj->yij", 
+            rr_ints, 
+            f0, 
+            r_ints_d, 
+            optimize=['einsum_path', (1, 2), (0, 1)],
+        )
+        x_x3_ints_f0 = numpy.einsum("xij,j->xij", r3_ints, f0)
+        x_x_ints_f0_yy_ints_d = numpy.einsum(
+                "xij,j,yj->xij", 
+                r_ints, 
+                f0, 
+                xx_ints_d, 
+                optimize=['einsum_path', (1, 2), (0, 1)],
+            )
+        x_y_ints_f0_y_ints_d_x_ints_d = numpy.einsum(
+                "xij,j,xj,yj->yij", 
+                r_ints, 
+                f0, 
+                r_ints_d, 
+                r_ints_d, 
+                optimize=['einsum_path', (1, 3), (1, 2), (0, 1)]
+            )
+        x_y_ints_f0_xy_ints_d = numpy.einsum(
+                "xij,j,xyj->yij", 
+                r_ints, 
+                f0, 
+                rr_ints_d, 
+                optimize=['einsum_path', (1, 2), (0, 1)]
+            )
+        x_x_ints_f0_y_ints_d_y_ints_d = numpy.einsum(
+                "xij,j,yj,yj->xij", 
+                r_ints, 
+                f0, 
+                r_ints_d, 
+                r_ints_d,  
+                optimize=['einsum_path', (2, 3), (1, 2), (0, 1)],
+            )
+        interm2 = 8 * (
+            x_yy_ints_f0_x_ints_d
+            + 2 * x_xy_ints_f0_y_ints_d 
+            - x_x3_ints_f0
+            + x_x_ints_f0_yy_ints_d # wrong prefactor in paper
+            - 6 * x_y_ints_f0_y_ints_d_x_ints_d
+            + 2 * x_y_ints_f0_xy_ints_d
+            - 3 * x_x_ints_f0_y_ints_d_y_ints_d # missing in paper
+        )
+
+        x_x_int_d_x_int_d = 4 * numpy.einsum("yi,yi->i", r_ints_d, r_ints_d)
+        xy_x_int_d_y_int_d = 8 * numpy.einsum("xi,yi->xyi", r_ints_d, r_ints_d)
+        
+        r_ints_f0_r_ints_d = 8 * numpy.einsum(
+            "xij,j,xj->ij", 
+            r_ints, 
+            f0, 
+            r_ints_d, 
+            optimize=['einsum_path', (1, 2), (0, 1)],
+        )
+        f0_r_ints_d_r_ints_d = 4 * numpy.einsum(
+            "j,yj,yj->j", 
+            f0, 
+            r_ints_d, 
+            r_ints_d,
+            optimize=['einsum_path', (1, 2), (0, 1)],
+        )
+        xy_x_ints_f0_y_ints_d = 16 * numpy.einsum(
+            "xij,j,yj->xyij", 
+            r_ints, 
+            f0, 
+            r_ints_d, 
+            optimize=['einsum_path', (1, 2), (0, 1)],
+        )
+        xy_f0_x_ints_d_y_ints_d = 8 * numpy.einsum(
+            "j,xj,yj->xyj",  
+            f0, 
+            r_ints_d, 
+            r_ints_d,
+            optimize=['einsum_path', (0, 1), (0, 1)],
+        )
+
+        x_f0_x_ints_d = 8 * numpy.einsum("j,xj->xj", f0, r_ints_d)
+        x_x_ints_f0 = 8 * numpy.einsum("xij,j->xij", r_ints, f0)
+
+        if self.exponent > 1:
+            xy_ints_x_ints_d_y_ints_d = numpy.einsum(
+                "xyij,xj,yj->ij",
+                rr_ints,  
+                r_ints_d, 
+                r_ints_d, 
+                optimize=['einsum_path', (1, 2), (0, 1)],
+            )
+            x_ints_xxx_intermed = numpy.einsum("xij,xj->ij", r_ints, rrr_intermed)
+            xx_ints_y_ints_d_y_ints_d = numpy.einsum(
+                "xij,yj,yj->ij", 
+                xx_ints, 
+                r_ints_d,
+                r_ints_d, 
+                optimize=['einsum_path', (1, 2), (0, 1)],
+            )
+            x3_ints_x_ints_d = numpy.einsum("xij,xj->ij", r3_ints, r_ints_d)
+            interm3 = (
+                2 * r4_ints
+                + 8 * xy_ints_x_ints_d_y_ints_d
+                - x_ints_xxx_intermed
+                + 4 * xx_ints_y_ints_d_y_ints_d
+                - 8 * x3_ints_x_ints_d
+            )
+
         # hessian vector product
         def h_op(x):
-            x = self.unpack_uniq_var(x).real
-
-            hx = numpy.zeros(2 * (self.mo_coeff.shape[1],), dtype=numpy.float64)
-            kappa_f1 = numpy.zeros(self.mo_coeff.shape[1], dtype=numpy.float64)
+            x = self.unpack_uniq_var(x)
 
             # terms containing f0
             # kappa @ x
             kappa_x = numpy.asarray([lib.dot(x, x_r_ints) for x_r_ints in r_ints])
+            kappa_x_d = kappa_x.diagonal(axis1=1, axis2=2).copy()
             if self.exponent > 1:
-                kappa_f1 += 8 * numpy.einsum(
-                    "xii,yyii,xii->i", kappa_x, rr_ints, r_ints
-                )
-                kappa_f1 -= 8 * numpy.einsum("xii,xii->i", kappa_x, r3_ints)
-                kappa_f1 -= 24 * numpy.einsum(
-                    "xii,xii,yii,yii->i", kappa_x, r_ints, r_ints, r_ints
-                )
-                kappa_f1 += 16 * numpy.einsum(
-                    "xii,xyii,yii->i", kappa_x, rr_ints, r_ints
-                )
-            kappa_x += numpy.einsum("xij->xji", kappa_x)
-            hx += numpy.einsum("xij,j,xj->ij", kappa_x, f0, rrr_intermed)
-            hx -= 8 * numpy.einsum("xxij,j,yjj,yjj->ij", rr_ints, f0, r_ints, kappa_x)
-            hx -= 16 * numpy.einsum("xyij,j,xjj,yjj->ij", rr_ints, f0, r_ints, kappa_x)
-            hx += 8 * numpy.einsum("xij,j,xjj->ij", r3_ints, f0, kappa_x)
-            hx -= 8 * numpy.einsum(
-                "xij,j,yyjj,xjj->ij", r_ints, f0, rr_ints, kappa_x
-            )  # wrong prefactor in paper
-            hx += 48 * numpy.einsum(
-                "xij,j,xjj,yjj,yjj->ij", r_ints, f0, r_ints, r_ints, kappa_x
-            )
-            hx -= 16 * numpy.einsum("xij,j,xyjj,yjj->ij", r_ints, f0, rr_ints, kappa_x)
-            hx += 24 * numpy.einsum(
-                "xij,j,yjj,yjj,xjj->ij", r_ints, f0, r_ints, r_ints, kappa_x
-            )  # missing in paper
+                kappa_f1 = numpy.einsum("xi,xi->i", kappa_x_d, interm1)
+            kappa_x += numpy.swapaxes(kappa_x, 1, 2)
+            kappa_x_d *= 2
+            hx = numpy.einsum("xij,xj->ij", kappa_x, x_f0_xxx_intermed)
+            hx -= numpy.einsum("xij,xj->ij", interm2, kappa_x_d)
 
             # kappa @ xy
             kappa_xy = numpy.empty_like(rr_ints)
             kappa_xy[idxs1, idxs2] = kappa_xy[idxs2, idxs1] = numpy.asarray(
                 [lib.dot(x, rr_ints[idx1, idx2]) for idx1, idx2 in zip(idxs1, idxs2)]
             )
+            kappa_xy_d = kappa_xy.diagonal(axis1=2, axis2=3).copy()
             if self.exponent > 1:
-                kappa_f1 += 4 * numpy.einsum(
-                    "xxii,yii,yii->i", kappa_xy, r_ints, r_ints
+                kappa_f1 += numpy.einsum("xxi,i->i", kappa_xy_d, x_x_int_d_x_int_d)
+                kappa_f1 += numpy.einsum(
+                    "xyi,xyi->i", kappa_xy_d, xy_x_int_d_y_int_d
                 )
-                kappa_f1 += 8 * numpy.einsum(
-                    "xyii,xii,yii->i", kappa_xy, r_ints, r_ints
-                )
-            kappa_xy += numpy.einsum("xyij->xyji", kappa_xy)
-            hx -= 8 * numpy.einsum("xij,j,xjj,yyjj->ij", r_ints, f0, r_ints, kappa_xy)
-            hx -= 4 * numpy.einsum("xxij,j,yjj,yjj->ij", kappa_xy, f0, r_ints, r_ints)
-            hx -= 16 * numpy.einsum("xij,j,yjj,xyjj->ij", r_ints, f0, r_ints, kappa_xy)
-            hx -= 8 * numpy.einsum("xyij,j,xjj,yjj->ij", kappa_xy, f0, r_ints, r_ints)
+            kappa_xy += numpy.swapaxes(kappa_xy, 2, 3)
+            kappa_xy_d *= 2
+            hx -= numpy.einsum("ij,yyj->ij", r_ints_f0_r_ints_d, kappa_xy_d)
+            hx -= numpy.einsum("xxij,j->ij", kappa_xy, f0_r_ints_d_r_ints_d)
+            hx -= numpy.einsum("xyij,xyj->ij", xy_x_ints_f0_y_ints_d, kappa_xy_d)
+            hx -= numpy.einsum("xyij,xyj->ij", kappa_xy, xy_f0_x_ints_d_y_ints_d)
 
             # kappa @ xxy
             kappa_xxy = numpy.asarray([lib.dot(x, x_r3_ints) for x_r3_ints in r3_ints])
+            kappa_xxy_d = kappa_xxy.diagonal(axis1=1, axis2=2).copy()
             if self.exponent > 1:
-                kappa_f1 -= 8 * numpy.einsum("xii,xii->i", kappa_xxy, r_ints)
-            kappa_xxy += numpy.einsum("xij->xji", kappa_xxy)
-            hx += 8 * numpy.einsum("xij,j,xjj->ij", kappa_xxy, f0, r_ints)
-            hx += 8 * numpy.einsum("xij,j,xjj->ij", r_ints, f0, kappa_xxy)
+                kappa_f1 -= 8 * numpy.einsum("xi,xi->i", kappa_xxy_d, r_ints_d)
+            kappa_xxy += numpy.swapaxes(kappa_xxy, 1, 2)
+            kappa_xxy_d *= 2
+            hx += numpy.einsum("xij,xj->ij", kappa_xxy, x_f0_x_ints_d)
+            hx += numpy.einsum("xij,xj->ij", x_x_ints_f0, kappa_xxy_d)
 
             # kappa @ xxyy
             kappa_xxyy = lib.dot(x, r4_ints)
             if self.exponent > 1:
-                kappa_f1 += 2 * numpy.einsum("ii->i", kappa_xxyy)
-            kappa_xxyy += numpy.einsum("ij->ji", kappa_xxyy)
+                kappa_f1 += 2 * kappa_xxyy.diagonal()
+            kappa_xxyy += kappa_xxyy.T
             hx -= 2 * numpy.einsum("ij,j->ij", kappa_xxyy, f0)
 
             # finish constructing kappa @ f1
@@ -281,21 +397,13 @@ class FourthMoment(boys.OrbitalLocalizer):
 
             # terms containing kappa @ f1
             if self.exponent > 1:
-                hx -= 2 * numpy.einsum("ij,j->ij", r4_ints, kappa_f1)
-                hx -= 8 * numpy.einsum(
-                    "xyij,j,xjj,yjj->ij", rr_ints, kappa_f1, r_ints, r_ints
-                )
-                hx += numpy.einsum("xij,j,xj->ij", r_ints, kappa_f1, rrr_intermed)
-                hx -= 4 * numpy.einsum(
-                    "xxij,j,yjj,yjj->ij", rr_ints, kappa_f1, r_ints, r_ints
-                )
-                hx += 8 * numpy.einsum("xij,j,xjj->ij", r3_ints, kappa_f1, r_ints)
+                hx -= numpy.einsum("ij,j->ij", interm3, kappa_f1)
 
             # multiply all terms by m
             hx *= self.exponent
 
             # term containing gradient
-            hx -= 0.5 * numpy.einsum("ij,ki->jk", full_g, x)
+            hx += 0.5 * lib.dot(x, full_g)
 
             return self.pack_uniq_var(hx - hx.T)
 
